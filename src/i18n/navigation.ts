8b950e40eb893d8b636ed1e557388256fa8800cb
Aquib Iqbal <aquibiqbal65@gmail.com>
2026-05-11 11:40:18 +0530

    /en format fix

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -2,4 +2,4 @@ import { createNavigation } from 'next-intl/navigation';
 import { locales, defaultLocale } from './config';
 
 export const { Link, redirect, usePathname, useRouter, permanentRedirect } =
-  createNavigation({ locales, defaultLocale });
+  createNavigation({ locales, defaultLocale, localePrefix: 'as-needed' });
