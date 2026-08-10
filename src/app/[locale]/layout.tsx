e18e7038c6b2639c03bee3a9535c8aaf969d9ff5
Aquib Iqbal <aquibiqbal65@gmail.com>
2026-08-11 03:31:20 +0530

    Add Microsoft Clarity tracking script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -295,6 +295,17 @@ export default async function LocaleLayout({
 
         <PerformanceScripts />
         {process.env.NEXT_PUBLIC_GA_ID && <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />}
+        {process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID && (
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
